99872e5e061f1e3fb3f18356f4a2615e95945ec7
zhaizhai <1044076780@qq.com>
2017-07-13 12:36:23 +0800

    demo3

---
+++ webpack-demo 3/app.js
@@ -0,0 +1,41 @@
+/*只要 input.value 被用户改了，data.newTodo 就会变成一样的值；*/
+/*只要 data.newTodo 被 JS 改了，input.value 就会变成一样的值。*/
+
+
+import Vue from 'vue'
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todoList: []
+  },
+  created: function(){
+    // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
+    window.onbeforeunload = ()=>{
+      let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
+      window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+    }
+
+    let oldDataString = window.localStorage.getItem('myTodos')
+    let oldData = JSON.parse(oldDataString)
+    this.todoList = oldData || []
+  },
+
+
+   methods: {
+    addTodo: function(){
+      this.todoList.push({
+        title: this.newTodo,
+        createdAt: new Date(),
+        done: false // 添加一个 done 属性
+      })
+      this.newTodo = ''
+    },
+    // 加了👇这个函数
+    removeTodo: function(todo){
+      let index = this.todoList.indexOf(todo) // Array.prototype.indexOf 是 ES 5 新加的 API
+      this.todoList.splice(index,1) // 不懂 splice？赶紧看 MDN 文档！
+    }
+  }
+  })
